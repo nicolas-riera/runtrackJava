063414064d36a92138358b3eb2bad00fd6efa878
nicolas-riera <nicolas.riera@laplateforme.io>
2026-03-30 11:40:25 +0200

    add: added job05

---
+++ jour01/job05.java
@@ -0,0 +1,26 @@
+package jour01;
+
+import java.util.Scanner;
+
+public class job05 {
+    public static void main(String[] args) {
+        Scanner lire = new Scanner(System.in);
+
+        System.out.print("Entrez un premier nombre : ");
+        Double nb1 = lire.nextDouble();
+
+        System.out.print("Entrez un deuxième nombre : ");
+        Double nb2 = lire.nextDouble();
+
+        System.out.print("Entrez un troisième nombre : ");
+        Double nb3 = lire.nextDouble();
+
+        if (nb1 > nb2 && nb1 > nb3) {
+            System.out.printf("Nombre maximun : %.2f", nb1);
+        } else if (nb2 > nb1 && nb2 > nb3) {
+            System.out.printf("Nombre maximun : %.2f", nb2);
+        } else {
+            System.out.printf("Nombre maximun : %.2f", nb3);
+        }
+    }
+}
