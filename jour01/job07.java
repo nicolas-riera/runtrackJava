e0ec0671ef2c3adaf2a0f694cd477e2d1cf127b0
nicolas-riera <nicolas.riera@laplateforme.io>
2026-03-30 11:47:33 +0200

    add: job07

---
+++ jour01/job07.java
@@ -0,0 +1,11 @@
+package jour01;
+
+public class job07 {
+    public static void main(String[] args) {
+        int somme = 0;
+        for (int i = 1; i <= 100; i++) {
+            somme = somme + i;
+        }
+        System.out.print(somme);
+    }
+}
