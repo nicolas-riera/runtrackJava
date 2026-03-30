4c7a77537eeade48a7470f0c74814116c35bfe7a
nicolas-riera <nicolas.riera@laplateforme.io>
2026-03-30 12:05:05 +0200

    add: added job09

---
+++ jour01/job09.java
@@ -0,0 +1,19 @@
+package jour01;
+
+import java.util.Scanner;
+
+public class job09 {
+    public static void main(String[] args) {
+    Scanner lire = new Scanner(System.in);
+
+    System.out.print("Entrez votre âge: ");
+    int age = lire.nextInt();
+    
+    if (age < 18) {
+        System.out.print("Vous êtes mineur");
+    } else {
+        System.out.print("Vous êtes majeur");
+    }
+    
+    }
+}
