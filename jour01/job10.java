3fcc52dda5a2e76f8aafd6176cf8220a713fe3a8
nicolas-riera <nicolas.riera@laplateforme.io>
2026-03-30 12:06:59 +0200

    add: added job10

---
+++ jour01/job10.java
@@ -0,0 +1,11 @@
+package jour01;
+
+public class job10 {
+    public static void main(String[] args) {
+        int factoriel = 1;
+        for (int i = 1; i <= 8; i++) {
+            factoriel = factoriel * i;
+        }
+        System.out.println(factoriel);
+    }
+}
