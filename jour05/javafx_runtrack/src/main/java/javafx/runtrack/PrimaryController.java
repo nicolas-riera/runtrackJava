02f154d6cd76a47e3d915e921737854ac4eae2cb
nicolas-riera <nicolas.riera@laplateforme.io>
2026-04-03 14:55:10 +0200

    made job06 (job05 was already done)

--- jour05/javafx_runtrack/src/main/java/javafx/runtrack/PrimaryController.java
+++ jour05/javafx_runtrack/src/main/java/javafx/runtrack/PrimaryController.java
@@ -3,6 +3,7 @@ package javafx.runtrack;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 
 public class PrimaryController {
@@ -14,10 +15,17 @@ public class PrimaryController {
     private Button sendButton;
 
     @FXML
-private TextField nomInput;
+    private TextField nomInput;
 
     @FXML
-    private void handleQuitAction() {
+    private MenuItem redButton;
+    @FXML
+    private MenuItem blueButton;
+    @FXML
+    private MenuItem greenButton;
+
+    @FXML
+    private void handleQuitAction() {   
         Platform.exit(); 
     }
 
@@ -25,5 +33,18 @@ private TextField nomInput;
     private void handleSendAction() {
         String valeur = nomInput.getText();
         System.out.println(valeur);
+    }
+
+    @FXML
+    private void handleRedButton() {
+        System.out.println("Red");
+    }
+    @FXML
+    private void handleBlueButton() {
+        System.out.println("Blue");
+    }
+    @FXML
+    private void handleGreenButton() {
+        System.out.println("Green");
     }
 } 
